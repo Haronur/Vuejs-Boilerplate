df932826ed15e1762fa8547564a8892e1bf6dc9a
HARON <haronur@gmail.com>
2020-05-11 22:49:02 +0600

    04-Named routes in Vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div id="app">
     <nav>
       <router-link to='/'>Home</router-link>
+      <router-link :to="{name: 'about'}">About</router-link>
       <router-link to='/register'>Register</router-link>
       <router-link to='/login'>Login</router-link>
-      <router-link to="/student/:id">Student</router-link>
+      <!-- <router-link to="/student/:id">Student</router-link> -->
+      <router-link :to="{name: 'student', params: {id: 2}}">Student</router-link>
     </nav>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from '../components/Home.vue';
+import About from '../components/About.vue';
 import Register from '../components/Register.vue';
 import Login from '../components/Login.vue';
-import Student from '../components/Student.vue';  
+import Student from '../components/Student.vue';
 
 Vue.use(Router);
 
@@ -12,9 +13,10 @@ const router = new Router({
 	mode: 'history',
     routes: [
     { path: '/', component: Home },
+    { path: '/about', component: About, name: 'about' },
     { path: '/register', component: Register },
     { path: '/login', component: Login },
-    { path: '/student/:id', component: Student },
+    { path: '/student/:id', component: Student, name: 'student' },
     ]
 });
 
